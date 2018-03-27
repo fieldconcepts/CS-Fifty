26a9b04c929a16b3b6173819bcd1ef63bf0ed916
Ben Sherwell <13998628+fieldconcepts@users.noreply.github.com>
2018-03-27 13:00:19 +0100

    Add files via upload

---
+++ pset8/application.py
@@ -0,0 +1,110 @@
+import os
+import re
+from flask import Flask, jsonify, render_template, request, url_for
+from flask_jsglue import JSGlue
+
+from cs50 import SQL
+from helpers import lookup
+
+# configure application
+app = Flask(__name__)
+JSGlue(app)
+
+# ensure responses aren't cached
+if app.config["DEBUG"]:
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
+
+# configure CS50 Library to use SQLite database
+db = SQL("sqlite:///mashup.db")
+
+@app.route("/")
+def index():
+    """Render map."""
+    if not os.environ.get("API_KEY"):
+        raise RuntimeError("API_KEY not set")
+    return render_template("index.html", key=os.environ.get("API_KEY"))
+
+@app.route("/articles")
+def articles():
+    """Look up articles for geo."""
+
+    #raise a runtime error if geotag is missing
+    if not request.args.get("geo"):
+        raise RuntimeError("missing geo code")
+
+    #takes the arguments in the url string http://pset8.cs50.net/articles?geo=99553 (in this case geo will be 99553)
+    geo = request.args.get("geo")
+
+    #inoput the geo value from url into the lookup function. This reurns a list of objects/dictionaries.
+    geo1 = lookup(geo)
+
+    #feed the list of dictnaries/aritcles into the jsonfiy function and print to webpage
+    return jsonify(geo1)
+
+@app.route("/search")
+def search():
+    """Search for places that match query."""
+
+    #check that a corerct post code or place name have been entered.
+    if not request.args.get("q"):
+        raise RuntimeError("missing zipcode")
+
+    #adds a wildcard string to end of post code or place name so more than 1 address is returned is using less characters. eg. 012 or 0123 or 01238. Cam or Cambridg or Cambridge
+    q = request.args.get("q") + "%"
+
+    #queries db and returned postal codes and places that are similar to the q arg from the get request
+    rows = db.execute("SELECT * FROM places WHERE postal_code LIKE :q OR place_name LIKE :q", q=q)
+
+    #crate and list of objects in a json file
+    return jsonify(rows)
+
+@app.route("/update")
+def update():
+    """Find up to 10 places within view."""
+
+    # ensure parameters are present
+    if not request.args.get("sw"):
+        raise RuntimeError("missing sw")
+    if not request.args.get("ne"):
+        raise RuntimeError("missing ne")
+
+    # ensure parameters are in lat,lng format
+    if not re.search("^-?\d+(?:\.\d+)?,-?\d+(?:\.\d+)?$", request.args.get("sw")):
+        raise RuntimeError("invalid sw")
+    if not re.search("^-?\d+(?:\.\d+)?,-?\d+(?:\.\d+)?$", request.args.get("ne")):
+        raise RuntimeError("invalid ne")
+
+    # explode southwest corner into two variables
+    (sw_lat, sw_lng) = [float(s) for s in request.args.get("sw").split(",")]
+
+    # explode northeast corner into two variables
+    (ne_lat, ne_lng) = [float(s) for s in request.args.get("ne").split(",")]
+
+    # find 10 cities within view, pseudorandomly chosen if more within view
+    if (sw_lng <= ne_lng):
+
+        # doesn't cross the antimeridian
+        rows = db.execute("""SELECT * FROM places
+            WHERE :sw_lat <= latitude AND latitude <= :ne_lat AND (:sw_lng <= longitude AND longitude <= :ne_lng)
+            GROUP BY country_code, place_name, admin_code1
+            ORDER BY RANDOM()
+            LIMIT 10""",
+            sw_lat=sw_lat, ne_lat=ne_lat, sw_lng=sw_lng, ne_lng=ne_lng)
+
+    else:
+
+        # crosses the antimeridian
+        rows = db.execute("""SELECT * FROM places
+            WHERE :sw_lat <= latitude AND latitude <= :ne_lat AND (:sw_lng <= longitude OR longitude <= :ne_lng)
+            GROUP BY country_code, place_name, admin_code1
+            ORDER BY RANDOM()
+            LIMIT 10""",
+            sw_lat=sw_lat, ne_lat=ne_lat, sw_lng=sw_lng, ne_lng=ne_lng)
+
+    # output places as JSON
+    return jsonify(rows)
